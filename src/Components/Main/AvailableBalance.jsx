e762be92d9d166a8788995d7d1e3c340b0e2de58
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-05 16:51:44 +0100

    Create AvailableBalance.jsx

---
+++ src/Components/Main/AvailableBalance.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import './styles.css'
+
+
+const AvailableBalance = () => {
+
+    return (
+        <section className="text-black bg-[#F6F6F6]">
+
+            <div className="">
+                <div className="balance bg- rounded-[1.5rem]">
+                    <h1 className="text-[rem] p-4 text-white leading-normal font-bold">
+                        AVAILABLE BALANCE
+                    </h1>
+
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default AvailableBalance;
