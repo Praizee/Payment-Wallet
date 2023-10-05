a92a8d94c6f84c56623643f72034bd14cba666ec
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-05 16:52:47 +0100

    Create NavbarData.jsx

---
+++ src/Components/Navbar/NavbarData.jsx
@@ -0,0 +1,35 @@
+import { PiBellLight } from "react-icons/pi";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { LuScanLine } from "react-icons/lu";
+
+
+export const NavbarData = [
+  {
+    image: PiBellLight,
+    path: "",
+    className: "",
+  },
+
+  {
+    image: IoChatboxEllipsesOutline,
+    path: "",
+    className: "",
+  },
+
+  {
+    image: LuScanLine,
+    path: "",
+    className: "",
+  }
+
+  // {
+  //   title: "Login",
+  //   path: "/login",
+  //   className: "laptop:hidden flex",
+  // },
+  // {
+  //   title: "Sign Up",
+  //   path: "/signup",
+  //   className: "laptop:hidden flex",
+  // }
+];
