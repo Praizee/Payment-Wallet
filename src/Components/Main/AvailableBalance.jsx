7f3a68718a53fe940a9e2283b34905b936584cfe
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-06 20:43:47 +0100

    Update AvailableBalance.jsx

--- src/Components/Main/AvailableBalance.jsx
+++ src/Components/Main/AvailableBalance.jsx
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
 import './styles.css'
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
 
 
 const AvailableBalance = () => {
 
     return (
-        <section className="text-black bg-[#F6F6F6]">
+        <section className="">
 
             <div className="">
-                <div className="balance bg- rounded-[1.5rem]">
-                    <h1 className="text-[rem] p-4 text-white leading-normal font-bold">
+                <div className="balance p-4 leading-normal rounded-[1.2rem] text-white">
+                    <p className="text-[0.75rem] font-semibold">
                         AVAILABLE BALANCE
-                    </h1>
-
+                    </p>
+                    <span className="flex space-x-3 text-[3.125rem]">
+                        {/* <span className="py-6">
+                            <AiOutlineEye size={30} title="Show" />
+                        </span> */}
+                        <span className="py-6">
+                            <AiOutlineEyeInvisible size={30} title="Hide" />
+                        </span>
+                        <h1>
+                            ₦#,###
+                        </h1>
+                    </span>
+                    <p className="text-[0.75rem] font-medium">
+                        *This balance is available to withdraw
+                    </p>
+                    <hr className="my-4 " />
+                    <p className="text-[0.75rem] font-semibold">
+                        LEDGER BALANCE
+                    </p>
+                    <span className="flex space-x-3 text-[2.1875rem]">
+                        {/* <span className="py-6">
+                            <AiOutlineEye size={30} title="Show" />
+                        </span> */}
+                        <span className="py-4">
+                            <AiOutlineEyeInvisible size={25} title="Hide" />
+                        </span>
+                        <h1>
+                            ₦#,###
+                        </h1>
+                    </span>
                 </div>
             </div>
 
-        </section>
+        </section >
     );
 };
 
