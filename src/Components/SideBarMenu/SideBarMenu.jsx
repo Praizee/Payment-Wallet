638f1edfb1477cef47659d8bb013ac1d5e26d463
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-09 16:58:47 +0100

    Changed button colors

--- src/Components/SideBarMenu/SideBarMenu.jsx
+++ src/Components/SideBarMenu/SideBarMenu.jsx
@@ -28,6 +28,7 @@ import {
     MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
 
+
 export function SidebarWithSearch() {
     const [open, setOpen] = React.useState(0);
     const [openAlert, setOpenAlert] = React.useState(true);
@@ -37,13 +38,8 @@ export function SidebarWithSearch() {
     };
 
     return (
-        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-            <div className="mb-2 flex items-center gap-4 p-4">
-                <img src="/img/logo-ct-dark.png" alt="brand" className="h-8 w-8" />
-                <Typography variant="h5" color="blue-gray">
-                    Sidebar
-                </Typography>
-            </div>
+        // <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] left-0 bg-transparent p-4 shadow-xl shadow-blue-gray-900/5">
             <div className="p-2">
                 <Input icon={<MagnifyingGlassIcon className="h-5 w-5" />} label="Search" />
             </div>
@@ -155,7 +151,8 @@ export function SidebarWithSearch() {
                     Log Out
                 </ListItem>
             </List>
-            <Alert open={openAlert} className="mt-auto" onClose={() => setOpenAlert(false)}>
+
+            <Alert open={openAlert} className="mt-auto bg-blue-gray-900" onClose={() => setOpenAlert(false)}>
                 <CubeTransparentIcon className="mb-4 h-12 w-12" />
                 <Typography variant="h6" className="mb-1">
                     Upgrade to PRO
@@ -169,12 +166,12 @@ export function SidebarWithSearch() {
                         as="a"
                         href="#"
                         variant="small"
-                        className="font-medium opacity-80"
+                        className="font-medium opacity-80 text-warning"
                         onClick={() => setOpenAlert(false)}
                     >
                         Dismiss
                     </Typography>
-                    <Typography as="a" href="#" variant="small" className="font-medium">
+                    <Typography as="a" href="#" variant="small" className="font-medium text-success">
                         Upgrade Now
                     </Typography>
                 </div>
