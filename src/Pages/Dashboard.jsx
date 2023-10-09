df10a339ac87756644f61e9b9bf6e771a3a8d7f5
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-09 16:59:13 +0100

    Added Sidebar menu component

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import AvailableBalance from "../Components/Main/AvailableBalance";
 import HeaderGraph from "../Components/Main/HeaderGraph";
 import Statistics from "../Components/Main/Statistics";
+import { SidebarWithSearch } from "../Components/SideBarMenu/SideBarMenu";
 
 const animationConfiguration = {
   initial: { opacity: 0 },
@@ -22,20 +23,27 @@ const Dashboard = () => {
         transition={{ duration: 3 }}
       >
 
-        <div className="container pt-20 pb-16 px-4 laptop:mr-[8rem]">
+        <div className="containe pt-24 pb-16 laptop:mr-[8rem]">
 
-          <div className="relative">
-            <h1 className="text-[2.5rem] text-[#2E2E3A] leading-normal font-bold">
-              Overview
-            </h1>
-
-            <div className="flex gap-5 my-4">
-              <HeaderGraph />
-              <AvailableBalance />
-            </div>
+          <div className="flex gap-2 justify-between relative">
 
             <div className="">
-              <Statistics />
+              <SidebarWithSearch />
+            </div>
+
+            <div className="relative">
+              <h1 className="text-[2.5rem] text-[#2E2E3A] leading-normal font-bold">
+                Overview
+              </h1>
+
+              <div className="flex gap-5 my-4">
+                <HeaderGraph />
+                <AvailableBalance />
+              </div>
+
+              <div className="">
+                <Statistics />
+              </div>
             </div>
 
           </div>
