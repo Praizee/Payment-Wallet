029873665702ccc1c2b0aa154fd7f742d9d4c55d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-09 14:54:31 +0100

    Added 'logged-in-user' profile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { FiMenu } from "react-icons/fi";
 import { LuBell } from "react-icons/lu";
 import { IoChatboxEllipsesOutline } from "react-icons/io5";
 import { LuScanLine } from "react-icons/lu";
+import UserPic from "../../assets/Userpic.png"
 
 
 const Navbar = () => {
@@ -26,19 +27,38 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-end gap-[0.05rem]">
-        <button className="btn btn-ghost btn-circle" title="Scan QR Code">
-          <LuScanLine size={23} />
-        </button>
-        <button className="btn btn-ghost btn-circle" title="Get Help">
-          <IoChatboxEllipsesOutline size={23} />
-        </button>
-        <button className="btn btn-ghost btn-circle" title="Notifications">
-          <div className="indicator">
-            <LuBell size={23} />
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button>
+      <div className="navbar-end gap-">
+        <div className="gap-[0.05rem]">
+          <button className="btn btn-ghost btn-circle" title="Scan QR Code">
+            <LuScanLine size={23} />
+          </button>
+          <button className="btn btn-ghost btn-circle" title="Get Help">
+            <IoChatboxEllipsesOutline size={23} />
+          </button>
+          <button className="btn btn-ghost btn-circle" title="Notifications">
+            <div className="indicator">
+              <LuBell size={23} />
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+        </div>
+
+        <div className="">
+          <span className="flex mx-4 gap-2 justify-between">
+            <span className="py-1">
+              <img src={UserPic} alt="User-pic" className="rounded-full" />
+            </span>
+
+            <span>
+              <h2 className="text-[#2E2E3A] text-[0.875rem] font-semibold">
+                Steve Ade
+              </h2>
+              <p className="text-[#9A9AAF] text-[0.75rem] font-light">
+                Admin
+              </p>
+            </span>
+          </span>
+        </div>
       </div>
 
     </div>
