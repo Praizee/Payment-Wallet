1831bac98ff90f85f62eadaa3638aa357b5c30bf
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-10 13:57:19 +0100

    Updated 'min-height' on wider screens

--- src/Components/Main/HeaderGraph.jsx
+++ src/Components/Main/HeaderGraph.jsx
@@ -6,7 +6,7 @@ const HeaderGraph = () => {
         <section className="">
             <div className="">
                 {/* min-w-[43.3125rem]  */}
-                <div className="p-4 min-w-[28.3125rem]  h-[24.4375rem] text-black bg-white rounded-lg">
+                <div className="p-4 min-w-[28.3125rem] laptop:min-w-[39rem]  h-[24.4375rem] text-black bg-white rounded-lg">
                     <span className="flex justify-between">
                         <span>
                             <p className="text-[0.75rem] text-[#2E2E3A] leading-normal font-bold">
