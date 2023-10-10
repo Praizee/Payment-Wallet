f880b9241f2928c394814515ae69525550158eba
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-10 15:12:39 +0100

    Created content.jsx for table data

---
+++ src/Components/Main/content.jsx
@@ -0,0 +1,49 @@
+import Cheque from "../../assets/cheque.png"
+import VisaRed from "../../assets/visa-red.png"
+import VisaBlue from "../../assets/visa-blue.png"
+import MasterCard from "../../assets/mastercard.png"
+import Wise from "../../assets/wise.png"
+
+export const CustomerTableData = [
+    {
+        customer: "Jenkins International Ltd.",
+        transactionID: "029830192",
+        channel: Cheque,
+        amount: "- ₦1,350",
+        date: "27/11/2022  4:13:00 PM",
+        status: "Pending",
+    },
+    {
+        customer: "Travail Clubs International",
+        transactionID: "924568334",
+        channel: VisaRed,
+        amount: "+ ₦18,125",
+        date: "23/10/2022  3:45:59 PM",
+        status: "Approved",
+    },
+    {
+        customer: "Google LLC",
+        transactionID: "394012490",
+        channel: VisaBlue,
+        amount: "- ₦1,350",
+        date: "27/9/2022  7:27:39 AM",
+        status: "Approved",
+    },
+    {
+        customer: "Meta Corps",
+        transactionID: "782019231",
+        channel: MasterCard,
+        amount: "+ ₦6,180",
+        date: "30/8/2022  11:01:53 PM",
+        status: "Approved",
+    },
+    {
+        customer: "Brumpost Media LLC",
+        transactionID: "112901928",
+        channel: Wise,
+        amount: "+ ₦39,000",
+        date: "28/7/2022  1:00:32 PM",
+        status: "Approved",
+    },
+
+]
