ff8d8869b8a4795214bf901278f33c8dfa09b082
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-18 16:00:45 +0100

    Create App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import { render } from '@testing-library/react'; // Import render function from testing library
+// import App from './App'; // Import the component you want to test
+
+// test('renders App component', () => {
+//     render(<App />); // Render your App component
+// });
