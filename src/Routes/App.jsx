e5a81ff2fc0a6be245742743403800709b5a14da
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-19 15:53:27 +0100

    Fixed BrowserRouter error

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import PublicLayout from "../Layouts/PublicLayout";
 import AuthenticatedLayout from "../Layouts/AuthenticatedLayout";
 import Contact from "../Pages/Contact/Contact";
