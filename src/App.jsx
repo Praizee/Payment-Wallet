92cb37a907423ca518c363c36bae25d444550f80
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-23 16:16:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from './Routes/App'; // Import the routing configuration
+import Routes from './Routes/App';
+import { AuthProvider } from './Context/AuthContext'; // Import the AuthProvider
 
 function App() {
     return (
         <Router>
-            <Routes />
+            {/* Wrap your app with the AuthProvider */}
+            <AuthProvider>
+                <Routes />
+            </AuthProvider>
         </Router>
     );
 }
