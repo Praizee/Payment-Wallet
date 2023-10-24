f6bf5cf67e155cf3696776d773c6c1fcba66d11c
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-24 15:30:59 +0100

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, updateProfile } from "firebase/auth";
+import { getAuth, updateProfile, sendPasswordResetEmail } from "firebase/auth";
 import { getDatabase, ref, set } from 'firebase/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
