46ae835ceaddeaaca2ac43277a385bf458fc41f0
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-30 16:03:47 +0100

    Included a 'loading' spinner on the submit button

--- src/Pages/ResetPassword/ResetPassword.jsx
+++ src/Pages/ResetPassword/ResetPassword.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import { auth } from '../../Firebase/firebase.js'; // Import Firebase authentication
+import { Spinner } from "@material-tailwind/react"; // Import the Spinner component
 
 const animationConfiguration = {
     initial: { opacity: 0 },
@@ -13,29 +14,28 @@ const animationConfiguration = {
 const ResetPassword = () => {
     const [email, setEmail] = useState("");
     const [isSent, setIsSent] = useState(false);
+    const [loading, setLoading] = useState(false); // Add loading state
 
     const handlePasswordReset = async () => {
+        setLoading(true); // Start loading
+
         try {
             await sendPasswordResetEmail(auth, email);
             console.log("Password reset email sent successfully.");
-            // You can add a success message or redirect the user to another page.
+            setIsSent(true); // Set isSent to true after successfully sending the email.
+
+            // Automatically hide the message after 5 seconds (5000 milliseconds)
+            setTimeout(() => {
+                setIsSent(false);
+            }, 5000);
         } catch (error) {
             console.error("Error sending password reset email:", error);
             // Handle the error, e.g., show an error message to the user.
+        } finally {
+            setLoading(false); // Stop loading
         }
-        sendPasswordResetEmail(auth, email)
-            .then(() => {
-                setIsSent(true); // Set isSent to true after successfully sending the email.
-
-                // Automatically hide the message after 5 seconds (5000 milliseconds)
-                setTimeout(() => {
-                    setIsSent(false);
-                }, 5000);
-            })
-            .catch((error) => {
-                // Handle any errors here.
-            });
     };
+
 
 
 
@@ -155,10 +155,18 @@ const ResetPassword = () => {
 
                                     <div className="col-span-6 sm:gap-4">
                                         <button
-                                            type="submit"
+                                            onClick={handlePasswordReset}
+                                            disabled={loading} // Disable the button while loading
                                             className="inline-block shrink-0 w-full rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
                                         >
-                                            Continue
+                                            {loading ? (
+                                                <span className="flex place-content-center gap-2">
+                                                    <Spinner color="blue" className="h-5 w-5 text-gray-500" />
+                                                    <span className="text-gray-800 text-sm">Sending Email... </span>
+                                                </span>
+                                            ) : (
+                                                'Send Reset Link'
+                                            )}
                                         </button>
 
                                     </div>
