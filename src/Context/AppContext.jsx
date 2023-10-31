15c76c10e5a418ab70590b996adcf14e2a270f76
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-10-31 09:56:58 +0100

    Included comments

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -3,19 +3,26 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { getDatabase, ref, get } from 'firebase/database';
 import { auth } from '../Firebase/firebase.js';
 
+// Create a context for the app's state
 const AppContext = createContext();
 
+// Create a custom hook for accessing the app's context
 export const useAppContext = () => {
     return useContext(AppContext);
 };
 
+// Define the AppProvider component that wraps the app and manages the state
 export const AppProvider = ({ children }) => {
+    // Initialize state variables for user data and loading status
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    // Use the useEffect hook to subscribe to authentication changes
     useEffect(() => {
+        // onAuthStateChanged listens for authentication state changes in Firebase
         const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
             if (authUser) {
+                // If a user is authenticated, retrieve their data from the database
                 const { uid, email } = authUser;
 
                 const db = getDatabase();
@@ -27,6 +34,7 @@ export const AppProvider = ({ children }) => {
                         const userData = snapshot.val();
                         const { first_name, last_name } = userData; // Use 'first_name' for retrieval
 
+                        // Update the user state with the retrieved data
                         setUser({
                             uid,
                             email,
@@ -44,14 +52,14 @@ export const AppProvider = ({ children }) => {
             } else {
                 setUser(null); // No authenticated user
             }
-            setLoading(false);
+            setLoading(false); // Set loading to false after handling authentication changes
         });
 
-        // Clean up the subscription
+        // Clean up the subscription to avoid memory leaks
         return () => unsubscribe();
     }, []);
 
-
+    // Provide the user and loading state as a value to the context
     return (
         <AppContext.Provider value={{ user, setUser, loading }}>
             {children}
